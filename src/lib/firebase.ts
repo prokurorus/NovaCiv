cb2c7ed707518aa3d930d9e3777817401cfbebc9
prokurorus <prokurorus2@gmail.com>
2025-11-19 12:24:17 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,1 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmTNXdBF7ilzeZVB2VaIt1USIMdXA2src",
+  authDomain: "novaciv-web.firebaseapp.com",
+  databaseURL: "https://novaciv-web-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "novaciv-web",
+  storageBucket: "novaciv-web.firebasestorage.app",
+  messagingSenderId: "884571454196",
+  appId: "1:884571454196:web:e88a75431190a9e299bca1",
+  measurementId: "G-FG254WGYF1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
 

0f4addd5e6416fde80fbdb1e93a469d7d54c7dff
prokurorus <prokurorus2@gmail.com>
2025-12-06 18:43:51 +0200

    Update process-video-jobs.js

--- netlify/functions/process-video-jobs.js
+++ netlify/functions/process-video-jobs.js
@@ -1,28 +1,9 @@
-// netlify/functions/process-video-jobs.js
-// Минимальный тестовый handler без зависимостей
-
-exports.handler = async function (event, context) {
-  try {
-    console.log("[process-video-jobs] minimal test started");
-
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      body: JSON.stringify({
-        ok: true,
-        message: "process-video-jobs minimal test OK",
-      }),
-    };
-  } catch (e) {
-    console.error("[process-video-jobs] error in minimal test:", e);
-
-    return {
-      statusCode: 500,
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      body: JSON.stringify({
-        ok: false,
-        error: e.message || String(e),
-      }),
-    };
-  }
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      ok: true,
+      message: "process-video-jobs minimal test OK"
+    })
+  };
 };
